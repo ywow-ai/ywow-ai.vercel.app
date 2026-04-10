f24d883b055cfd8131278108b09ebdb7ebd9b76b
ywow-ai <masyaqin37@gmail.com>
2026-04-10 15:39:29 +0700

    style: Minor UI tweaks (UI: Rounded corners adjusted; Spinner: Stroke width prop added; Drawer: Adjusted width)

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -2,11 +2,15 @@ import { cn } from "@/lib/utils";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Loading03Icon } from "@hugeicons/core-free-icons";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({
+  className,
+  strokeWidth,
+  ...props
+}: React.ComponentProps<"svg">) {
   return (
     <HugeiconsIcon
       icon={Loading03Icon}
-      strokeWidth={2}
+      strokeWidth={strokeWidth ? Number(strokeWidth) : 2}
       role="status"
       aria-label="Loading"
       className={cn("size-4 animate-spin", className)}
